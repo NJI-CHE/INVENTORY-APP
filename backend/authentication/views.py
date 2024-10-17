b4c9762685d9f6d117747f573288d7a2db4bbb93
NJI-CHE <njicornelius@gmail.com>
2024-10-17 21:49:38 +0100

    Added cookies

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -8,7 +8,34 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
+from rest_framework_simplejwt.views import(
+    TokenObtainPairView,
+    TokenRefreshView
+
+)
+
 # Create your views here.
+class CustumTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        
+        try:
+            response = super().post(request, *args, **kwargs)
+            tokens = request.data
+
+            access_token = tokens['access']
+            refresh_token = token['refresh']
+
+            res = Response()
+
+            res.data = {"success":True}
+
+            res.set_cookie(
+                key="access_token",
+                value= access_token,
+                httponly=True,
+                secure=True
+
+            )
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
